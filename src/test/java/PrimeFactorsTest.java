317bc871c19f4c0e2107358b760b40c3fbc4982f
jnuank <ikuta1919@gmail.com>
2025-01-10 23:02:32 +0900

    3まで通った

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -3,17 +3,28 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrimeFactorsTest {
     @Test
     public void factors() throws Exception {
-        assertTrue(factorsOf(1).contains(2));
+        assertEquals(List.of(2), factorsOf(2));
     }
 
-    private List<Integer> factorsOf(int i) {
+    @Test
+    public void factors3() {
+        assertEquals(List.of(3), factorsOf(3));
+    }
+
+    @Test
+    public void 素数分析4の場合() {
+        assertEquals(List.of(2,2), factorsOf(4));
+    }
+
+    private List<Integer> factorsOf(int n) {
         var factors = new ArrayList<Integer>();
-        factors.add(2);
+        if (n > 1)
+            factors.add(n);
         return factors;
     }
 }
