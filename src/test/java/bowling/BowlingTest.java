3115adaecc6d27d1143716aa65e56eb3a0bec863
jnuank <ikuta1919@gmail.com>
2025-01-11 17:55:15 +0900

    Gameクラス作成をsetupに持っていく

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -1,10 +1,17 @@
 package bowling;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BowlingTest {
+    private Game game;
+    @BeforeEach
+    public void setUp() {
+        game = new Game();
+    }
+
     @Test
-    public void Game作成() {
-        Game g = new Game();
+    public void Rollする() {
+        game.roll(0);
     }
 }
