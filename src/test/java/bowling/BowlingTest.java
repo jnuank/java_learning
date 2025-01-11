d80420cbaf063eb09e15a36ccbe727d85d67fbfd
jnuank <ikuta1919@gmail.com>
2025-01-11 17:57:52 +0900

    スコア計算

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -3,6 +3,8 @@ package bowling;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BowlingTest {
     private Game game;
     @BeforeEach
@@ -13,5 +15,14 @@ public class BowlingTest {
     @Test
     public void Rollする() {
         game.roll(0);
+    }
+
+    @Test
+    public void スコア計算する() {
+        for (int i=0; i<20; i++) {
+            game.roll(0);
+        }
+
+        assertEquals(0, game.score());
     }
 }
