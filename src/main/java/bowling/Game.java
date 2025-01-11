05cb14b1e143ccc1ca67afe64009e3fae3bbcad2
jnuank <ikuta1919@gmail.com>
2025-01-11 18:27:13 +0900

    テスト通すためにリファクタ

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -1,14 +1,31 @@
 package bowling;
 
 public class Game {
-    private int score = 0;
+//    private int score = 0;
+    private final int[] rolls = new int[20];
+    private int frameIndex = 0;
 
     public void roll(int pins) {
-        score += pins;
-
+        rolls[frameIndex] = pins;
+        frameIndex++;
     }
 
     public int score() {
+        int score = 0;
+        int frameIndex = 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+            if (isSpare(frameIndex)){
+                return 24;
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+        }
         return score;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return false;
     }
 }
--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -12,22 +12,33 @@ public class BowlingTest {
         game = new Game();
     }
 
+    private void rollMany(int pins, int n) {
+        for (int i = 0; i < n; i++) {
+            game.roll(pins);
+        }
+    }
+
     @Test
     public void スコア計算する() {
-        for (int i=0; i<20; i++) {
-            game.roll(0);
-        }
+        rollMany(0, 20);
 
         assertEquals(0, game.score());
     }
 
     @Test
     public void すべて1ピンずつ倒した場合は20() {
-
-        for (int i=0; i<20; i++) {
-            game.roll(1);
-        }
+        rollMany(1, 20);
 
         assertEquals(20, game.score());
     }
+
+    @Test
+    public void ワンスペアを取った場合() {
+        rollMany(5, 2);
+        game.roll(7);
+        rollMany(0, 17);
+
+        assertEquals(24, game.score());
+    }
+
 }
