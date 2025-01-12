65e789c7fc96b869e34a21da11ba3bf00ca678ec
jnuank <ikuta1919@gmail.com>
2025-01-12 19:33:43 +0900

    setupに初期化は移す

--- src/test/java/gossipBus/GossipTest.java
+++ src/test/java/gossipBus/GossipTest.java
@@ -1,19 +1,23 @@
 package gossipBus;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GossipTest {
+    private Driver driver1;
+    @BeforeEach
+    public void setUp() {
+        driver1 = new Driver();
+    }
     @Test
     public void ドライバーが最初に止まるバス停はstop1() {
-        var driver1 = new Driver();
         assertEquals("stop1", driver1.getStop());
     }
 
     @Test
     public void ドライバーが2番目に止まるバス停はstop2() {
-        var driver1 = new Driver();
         driver1.drive(); // 何も返さないのって気持ち悪いなぁ
         assertEquals("stop2", driver1.getStop());
     }
